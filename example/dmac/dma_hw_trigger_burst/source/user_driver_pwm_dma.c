fa13b74b17747a6291750da7878e457cc719467a
qqq5127 <459507540@qq.com>
2021-06-23 14:54:15 +0800

    feat(spi + DMA):完成DMA+SPI驱动

--- example/dmac/dma_hw_trigger_burst/source/user_driver_pwm_dma.c
+++ example/dmac/dma_hw_trigger_burst/source/user_driver_pwm_dma.c
@@ -8,12 +8,14 @@
 #include "spi.h"
 #include "user_driver_pwm_dma.h"
 
+
+#define	SPI_DMA_CONFIG		1
 /******************************************************************************
 * Local variable definitions ('static')                                      *
 ******************************************************************************/
 //static uint8_t* io_point = NULL;	// cuixu test
-static uint8_t  pwm_led_buff[40][3][8];
-boolean_t bSendIrq,bRecvIrq;
+static uint32_t  pwm_led_buff[40][24];
+boolean_t bSendIrq;
 
 
 static void Spi0CallBack(void)
@@ -24,11 +26,16 @@ static void Spi0CallBack(void)
         Spi_ClearStatus(Spi0);
         return;
     }
-		if(Spi_GetStatus(Spi0,SpiTxe))
+    if(Spi_GetStatus(Spi0,SpiRxne))
+    {
+      Spi_FuncEn(Spi0,SpiRxNeIe,FALSE);
+      Spi_ClearStatus(Spi0);
+    }
+		else if(Spi_GetStatus(Spi0,SpiTxe))
     {
       bSendIrq = 1;
-      Spi_FuncEn(Spi0,SpiTxEIe,FALSE);
-      Spi_ClearStatus(Spi0);
+			//Spi_FuncEn(Spi0,SpiTxEIe,FALSE);
+			//Spi_ClearStatus(Spi0);
     }
 }
 
@@ -49,11 +56,12 @@ void Spi_PortInit(void)
     stcGpioCfg.enDir = GpioDirOut;   
     Gpio_Init(GpioPortB, GpioPin5,&stcGpioCfg);//MOSI
     Gpio_SetAfMode(GpioPortB, GpioPin5,GpioAf1);   
-    Gpio_Init(GpioPortA, GpioPin15,&stcGpioCfg);//CS
-    Gpio_SetAfMode(GpioPortA, GpioPin15,GpioAf1);
+    Gpio_Init(GpioPortA, GpioPin4,&stcGpioCfg);//CS
+    Gpio_SetAfMode(GpioPortA, GpioPin4,GpioAf1);
     Gpio_Init(GpioPortB, GpioPin3,&stcGpioCfg);//SCK
     Gpio_SetAfMode(GpioPortB, GpioPin3,GpioAf1);
     stcGpioCfg.enDir = GpioDirIn;
+    stcGpioCfg.enPuPd = GpioPd;
     Gpio_Init(GpioPortA, GpioPin11,&stcGpioCfg);//MISO
     Gpio_SetAfMode(GpioPortA, GpioPin11,GpioAf6);
 }
@@ -63,18 +71,21 @@ void user_driver_pwm_dma_init(void)
 {
 
 	en_flash_waitcycle_t          enFlashWait;
-	//stc_dma_config_t           stcDmaCfg;    
   stc_sysctrl_clk_config_t      stcClkConfig;
 	stc_gpio_config_t 				stcLEDPortCfg;
 	stc_spi_config_t  SPIConfig;		
+
+#if SPI_DMA_CONFIG	
 	stc_dma_config_t           stcDmaCfg;
-  //DDL_ZERO_STRUCT(stcDmaCfg);   
+#endif
+
   DDL_ZERO_STRUCT(stcClkConfig);
 	DDL_ZERO_STRUCT(stcLEDPortCfg);
 	DDL_ZERO_STRUCT(pwm_led_buff);
 	DDL_ZERO_STRUCT(SPIConfig);
+#if SPI_DMA_CONFIG	
 	DDL_ZERO_STRUCT(stcDmaCfg); 
-
+#endif
 
 
 	stcClkConfig.enClkSrc 	 = SysctrlClkXTH; 					//使用外部高速晶振,32M
@@ -88,19 +99,31 @@ void user_driver_pwm_dma_init(void)
 	
 	Sysctrl_SetPeripheralGate(SysctrlPeripheralGpio, TRUE); //GPIO 外设时钟使能
 	Sysctrl_SetPeripheralGate(SysctrlPeripheralSpi0,TRUE); //SPI0
+	Sysctrl_SetPeripheralGate(SysctrlPeripheralDma,TRUE); //SPI0
 
 	{
-		uint8_t i,j,k;
-		
-		for(i=0;i<40;i++)
+		boolean_t flag = FALSE;
+		for(uint8_t i=0;i<40;i++)
 		{
-			for(j=0;j<3;j++)
-				for(k=0;k<8;k++)
-					{
-						pwm_led_buff[i][j][k]=0xaa;
-					}
+			for(uint8_t j=0;j<24;j++)
+			{
+			#if 0
+				if(flag)
+				{
+					pwm_led_buff[i][j]=0xC0;	//0
+				}
+				else
+				{
+					pwm_led_buff[i][j]=0xFC;	//1
+				}
+				flag = !flag;
+			#else
+				pwm_led_buff[i][j]=0xFC;	//0
+			#endif
+			}
 		}
-
+		pwm_led_buff[0][7]=0;
+		pwm_led_buff[39][23]=0;
 	}
 #if 0
 	stcLEDPortCfg.enDir  = GpioDirOut;
@@ -114,56 +137,45 @@ void user_driver_pwm_dma_init(void)
 		*(io_point) = 0x00;
 	}
 #endif
-	
+
 	Spi_PortInit();//模块初始化
-	Spi_SetCS(Spi0,TRUE);
 	//SPI模块配置
 	SPIConfig.bCPHA = Spicphafirst;//模式0
 	SPIConfig.bCPOL = Spicpollow;
-	SPIConfig.bIrqEn = TRUE;
+	SPIConfig.bIrqEn = FALSE;
 	SPIConfig.bMasterMode = SpiMaster;
-	SPIConfig.u8BaudRate = SpiClkDiv8;
+	SPIConfig.u8BaudRate = SpiClkDiv2;
 	SPIConfig.pfnSpi0IrqCb = Spi0CallBack;
-	if(SPIConfig.bIrqEn)
-	{
-		EnableNvic(SPI0_IRQn,IrqLevel3,TRUE);
-	}
 	Spi_Init(Spi0,&SPIConfig);//模块初始化
-
 	Spi_SetCS(Spi0,FALSE);//使能片选信号
-	//while(1)
-
-
-  //使能DMA触发
-  Adc_ConfigDmaTrig(DmaSqr);
-  
-  stcDmaCfg.enMode =  DmaBurst;                   
+	
+	
+	Spi_FuncEn(Spi0,SpiDmaTxEn,TRUE);	
+	
+#if SPI_DMA_CONFIG	
+  stcDmaCfg.enMode =  DmaBlock;                   
   stcDmaCfg.u16BlockSize = 0x01u;
-  stcDmaCfg.u16TransferCnt = 40*3*8;         //burst模式，一次触发传输数据大小为 3*1,
-  stcDmaCfg.enTransferWidth = Dma8Bit;            
-  stcDmaCfg.enSrcAddrMode = AddressFix;
-  stcDmaCfg.enDstAddrMode = AddressIncrease;
+  stcDmaCfg.u16TransferCnt = 40*24 + 1;         //burst模式，一次触发传输数据大小
+  stcDmaCfg.enTransferWidth = Dma32Bit;            
+  stcDmaCfg.enSrcAddrMode = AddressIncrease;
+  stcDmaCfg.enDstAddrMode = AddressFix;
   stcDmaCfg.bDestAddrReloadCtl = FALSE;
-  stcDmaCfg.bSrcAddrReloadCtl = FALSE;
-  stcDmaCfg.bSrcBcTcReloadCtl = FALSE;
-  stcDmaCfg.u32SrcAddress = (uint32_t) &(M0P_SPI0->DATA);
-  stcDmaCfg.u32DstAddress = (uint32_t)&pwm_led_buff;
-  stcDmaCfg.enRequestNum = SPI0TXTrig;	        //设置为ADCSQR触发
+  stcDmaCfg.bSrcAddrReloadCtl = TRUE;
+  stcDmaCfg.bSrcBcTcReloadCtl = TRUE;
+  stcDmaCfg.u32SrcAddress = (uint32_t)&pwm_led_buff[0][0];
+  stcDmaCfg.u32DstAddress = (uint32_t) &(M0P_SPI0->DATA);
+  stcDmaCfg.enRequestNum = SPI0TXTrig;	        //设置为SPI 触发
   Dma_InitChannel(DmaCh0,&stcDmaCfg);	
-  
-  //使能DMA，使能DMA0，启动DMA0
+		
+	//M0P_SPI0->DATA = 0xaa;
+	//使能DMA，使能DMA0，启动DMA0
   Dma_Enable();
   Dma_EnableChannel(DmaCh0);
-  Dma_Start(DmaCh0);		
-  //Adc_SQR_Start();
-	{
-		Spi_SendData(Spi0,0xAA);//发送数据
-	}
-
+	//EnableNvic(SPI0_IRQn,IrqLevel3,TRUE);
+	//M0P_SPI0->DATA = 0xaa;
   //等待传输完成
-  //while(Dma_GetStat(DmaCh0) != DmaTransferComplete);
-	while(1)
-	{
-		Spi_SendData(Spi0,0xAA);//发送数据
-	}
+  while(Dma_GetStat(DmaCh0) != DmaTransferComplete);
+#endif	
+	Spi_SetCS(Spi0,TRUE);//使能片选信号
+
 }
